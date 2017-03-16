06b52b1035edadcb64e75b29b4e2223ccc3196a2
Gabriel Faundez <gnfaundez@uc.cl>
2017-03-16 16:43:18 -0300

    AC03

--- Actividades/AC03/AC03.py
+++ Actividades/AC03/AC03.py
@@ -1,6 +1,6 @@
 # AC03 -Actividad en clases
 from collections import deque
-
+from random import choice
 
 class Camion:
     def __init__(self, capacidad_maxima, urgencia):
@@ -27,12 +27,12 @@ class Camion:
         string = ""
         dic = {}
         for producto in self.productos:
-            if dic[producto.tipo] is None:
+            if producto.tipo in dic:
                 dic[producto.tipo] = 1
             else:
                 dic[producto.tipo] += 1
         for tipo, cantidad in self.dic.items():
-            string += "{} : {}".format(tipo, cantidad)
+            string += "{} : {}\n".format(tipo, cantidad)
         return string
 
 
@@ -40,6 +40,7 @@ class CentrosDistribucion:
     def __init__(self):
         self.fila = deque()
         self.bodega = {}
+        self.numeroCamiones = 0
         with open("camiones.txt","r") as camiones:
             for camion in camiones:
                 camion = camion.strip()
@@ -60,24 +61,35 @@ class CentrosDistribucion:
                     break
 
     def rellenar_camion(self):
-        self.fila[0].productos.append()
+        for almacen in self.bodega.values():
+            for pila in almacen.values():
+                if pila[-1].peso + self.fila[0].peso_acumulado < self.fila[0].capacidad_maxima:
+                    self.fila[0].agregar_producto(pila.pop())
+        self.fila[0].llenado = True
 
-        pass
 
     def enviar_camion(self):
         if(self.fila[0].llenado == True):
-            pass
+            print("camion numero {} enviado\n contenido {}".format(self.numeroCamiones,self.fila[0]))
+            self.fila.popleft()
+            self.numeroCamiones += 1
         else:
-            pass
+            print("Camion no se ha llenado aun")
         pass
 
     def mostrar_productos_por(self,tipo):
+        string = "mostrando objetos del tipo: ".format(tipo)
+        for i in self.bodega[tipo].values():
+            for j in i:
+                string += "{}\n".format(j.nombre)
         pass
 
-    def recibir_donacion(self,**kwargs):
-        for i in kwargs:
-            if():
-        pass
+    def recibir_donacion(self,*args):
+        for producto in args:
+            if producto.tipo in self.bodega:
+                self.bodega[producto.tipo][producto.nombre].append(producto)
+            else:
+                self.bodega[producto.tipo] = {producto.nombre : [producto]}
 
 class Producto:
     def __init__(self,nombre , tipo, peso):
