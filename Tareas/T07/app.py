4e133a2ac08ac9a38608405d16f621908d4ee1e7
Gabriel Faundez <gnfaundez@uc.cl>
2017-06-21 17:51:42 -0400

    Automatic commit heroku deploy

--- Tareas/T07/app.py
+++ Tareas/T07/app.py
@@ -1,6 +1,3 @@
-import requests
-import flask
-
 with open("telegram_token", "r") as file:
     T_TOKEN = file.read().strip()
 with open("github_token", "r") as file:
@@ -9,17 +6,3 @@ with open("github_token", "r") as file:
 URL_TEL_BOT = "https://api.telegram.org/bot{token}".format(**{"token": T_TOKEN})
 URL_GIT = "https://api.github.com/user"
 URL_GOOGLE = "http://www.google.com/search?"
-
-print(requests.get(URL_TEL_BOT + "/GetUpdates").json())
-requests.get(URL_TEL_BOT + "/sendMessage", params={"chat_id": 413925182, "text": "new_deploy"}).json()
-app = flask.Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "<h1>I'm a bot</h1>"
-
-
-@app.route("/payload/<load>")
-def github(load):
-    return requests.get(URL_TEL_BOT + "/sendMessage", params={"chat_id": 413925182, "text": load}).json()
--- Tareas/T07/main.py
+++ Tareas/T07/main.py
@@ -1,5 +1,7 @@
 import flask
 import requests
+import re
+from flask import request
 
 with open("telegram_token", "r") as file:
     T_TOKEN = file.read().strip()
@@ -7,6 +9,8 @@ with open("telegram_token", "r") as file:
 URL_TEL_BOT = "https://api.telegram.org/bot{token}".format(**{"token": T_TOKEN})
 
 requests.get(URL_TEL_BOT + "/sendMessage", params={"chat_id": 413925182, "text": "new_deploy"}).json()
+requests.get(URL_TEL_BOT + "/setWebhook", params={"url": "https://drmavrakis4ever.herokuapp.com/telegram",
+                                                  "allowed_updates": ["message"]})
 
 app = flask.Flask(__name__)
 
@@ -16,9 +20,29 @@ def index():
     return "<h1>I'm a bot</h1>"
 
 
-@app.route("/payload/<load>")
-def github(load):
-    req = requests.get(URL_TEL_BOT + "/sendMessage", params={"chat_id": 413925182, "text": load})
-    return req.status_code
+@app.route("/admin/<message>")
+def admin(message):
+    req = requests.get(URL_TEL_BOT + "/sendMessage", params={"chat_id": 413925182, "text": message})
+    return "<p>{}</p>".format(req.status_code)
+
+
+@app.route("/payload", methods=["POST"])
+def github():
+    data = request.json
+    admin("GitHub say something")
+    return "200 OK"
+
+
+@app.route("/telegram", methods=["POST"])
+def telegram():
+    data = request.json
+    if 'entities' in data['message']:
+        from_data = data['message']['from']
+        chat_data = data['message']['chat']
+        text = data['message']['text']
+        if bool(re.match("\/(get #[0-9]+|post #[0-9]+ \*[\w \n]+|label #[0-9]+ [\w ]+|close #[0-9]+)", text)):
+            admin("I receive a command")
+
+    return "200 OK"
 
 # app.run(port="")
