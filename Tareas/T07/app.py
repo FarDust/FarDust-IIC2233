b563302ad11fcac08bf5a5232b1c3602d4bc7288
Gabriel Faundez <gnfaundez@uc.cl>
2017-06-21 12:50:45 -0400

    Automatic commit heroku deploy

---
+++ Tareas/T07/app.py
@@ -0,0 +1,29 @@
+import requests
+import flask
+
+with open("telegram_token", "r") as file:
+    T_TOKEN = file.read().strip()
+with open("github_token", "r") as file:
+    G_TOKEN = file.read().strip()
+
+URL_TEL_BOT = "https://api.telegram.org/bot{token}".format(**{"token": T_TOKEN})
+URL_GIT = "https://api.github.com/user"
+URL_GOOGLE = "http://www.google.com/search?"
+
+print(requests.get(URL_TEL_BOT + "/GetUpdates").json())
+print(requests.get(URL_TEL_BOT + "/sendMessage", params={"chat_id": 413925182, "text": "test"}).json())
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>I'm a bot</h1>"
+
+
+@app.route("/payload/<load>")
+def github(load):
+    return print(load)
+
+
+if __name__ == '__main__':
+    app.run(port=4567)
--- Tareas/T07/main.py
+++ Tareas/T07/main.py
@@ -1,15 +1,1 @@
-import requests
-import flask
-
-with open("telegram_token", "r") as file:
-    T_TOKEN = file.read().strip()
-with open("github_token", "r") as file:
-    G_TOKEN = file.read().strip()
-
-URL_TEL_BOT = "https://api.telegram.org/bot{token}".format(**{"token": T_TOKEN})
-URL_GIT = "https://api.github.com/user"
-URL_GOOGLE = "http://www.google.com/search?"
-
-print(requests.get(URL_TEL_BOT + "/getMe").json())
-print(requests.get(URL_GOOGLE, params={"q": "chile"}))
-#print(requests.get(URL_GIT, auth=G_TOKEN).url)
+import app
