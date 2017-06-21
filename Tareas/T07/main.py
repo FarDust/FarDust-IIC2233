b6fad49bc2074788ec342341c416700eda18ae08
Gabriel Faundez <gnfaundez@uc.cl>
2017-06-21 11:55:41 -0400

    Automatic commit heroku deploy

--- Tareas/T07/main.py
+++ Tareas/T07/main.py
@@ -1,6 +1,15 @@
-from requests import request, get, post
+import requests
+import flask
 
-URL_TEL_BOT = "https://api.telegram.org/bot423073312:AAE12wJM9zk2LF3NVSLe-LFRwy78kYDX0Js"
+with open("telegram_token", "r") as file:
+    T_TOKEN = file.read().strip()
+with open("github_token", "r") as file:
+    G_TOKEN = file.read().strip()
 
-print(get(URL_TEL_BOT+"/getMe").json())
+URL_TEL_BOT = "https://api.telegram.org/bot{token}".format(**{"token": T_TOKEN})
+URL_GIT = "https://api.github.com/user"
+URL_GOOGLE = "http://www.google.com/search?"
 
+print(requests.get(URL_TEL_BOT + "/getMe").json())
+print(requests.get(URL_GOOGLE, params={"q": "chile"}))
+#print(requests.get(URL_GIT, auth=G_TOKEN).url)
