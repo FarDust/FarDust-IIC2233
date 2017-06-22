75a995505f0bacfc26509b8d661500ddc08af1db
Gabriel Faundez <gnfaundez@uc.cl>
2017-06-22 19:14:55 -0400

    Automatic commit heroku deploy

--- Tareas/T07/main.py
+++ Tareas/T07/main.py
@@ -107,7 +107,8 @@ def create_comment(number, message, chat):
     req = requests.post(url=URL_GIT.format(number)+"/comments", params={"access_token": G_TOKEN}, json={"body": message})
     if req.status_code == 201:
         message = "Se ha comentado en la issue #{}".format(number)
-        message = message_format(message, req)
+        getter = requests.get(url=URL_GIT.format(number), params={"access_token": G_TOKEN})
+        message = message_format(message, getter)
     elif req.status_code == 404:
         message = "Esa issue no existe"
     else:
