aaf2160eab982b45022e730770792e2c26a64c17
Gabriel Faundez <gnfaundez@uc.cl>
2017-06-22 18:45:19 -0400

    Automatic commit heroku deploy

--- Tareas/T07/main.py
+++ Tareas/T07/main.py
@@ -104,11 +104,10 @@ def label_issue(number, label, chat):
 
 
 def create_comment(number, message, chat):
-    print(number,message,chat)
     req = requests.patch(url=URL_GIT.format(number)+"/comments", params={"access_token": G_TOKEN},
                          data=flask.json.dumps({'body': message}))
-    print(req.url)
-    if req.status_code == 200:
+    print(req.status_code)
+    if req.status_code == 201:
         message = "Se ha comentado en la issue #{}".format(number)
         message = message_format(message, req)
     elif req.status_code == 404:
